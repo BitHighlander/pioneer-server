4822cc39f426d657a298e3d7d294bc1f6ad151ea
highlander <bithighlander@gmail.com>
2021-07-16 23:05:25 -0600

    debug make

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,2 +1,3 @@
-for dir in services/*; do (cd "$dir" && npm i && npm run build); done
-for dir in services/*/*; do (cd "$dir" && npm i && npm run build); done
+for dir in services/rest; do (cd "$dir" && npm i && npm run build); done
+for dir in services/events/*; do (cd "$dir" && npm i && npm run build); done
+for dir in services/ingesters/*; do (cd "$dir" && npm i && npm run build); done
