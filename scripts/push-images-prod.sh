71283dfef664e4b1a34b90cc8f49a5386b82d894
highlander <bithighlander@gmail.com>
2021-07-16 22:47:45 -0600

    push everything

--- scripts/push-images-prod.sh
+++ scripts/push-images-prod.sh
@@ -1,3 +1,3 @@
 #https://unix.stackexchange.com/questions/103920/parallelize-a-bash-for-loop
-for dir in services/*; do (cd "$dir" && npm i && npm run docker:push:all) & done
+for dir in services/*/*; do (cd "$dir" && npm i && npm run docker:push:all) & done
 wait
