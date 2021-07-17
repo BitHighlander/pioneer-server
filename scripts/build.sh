f9bcdfb78a68a7faf9f76ad39aae690241c86a3a
highlander <bithighlander@gmail.com>
2021-07-16 23:25:10 -0600

    debug make

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,3 +1,10 @@
-for dir in services/rest; do (cd "$dir" && npm i && npm run build); done
-for dir in services/events/*; do (cd "$dir" && npm i && npm run build); done
-for dir in services/ingesters/*; do (cd "$dir" && npm i && npm run build); done
+#verbose
+cd services/rest && npm i && npm run build; cd ../..;
+cd services/ingesters/pubkeys && npm i && npm run build; cd ../../..;
+cd services/ingesters/txs/high && npm i && npm run build; cd ../../..;
+cd services/ingesters/txs/low && npm i && npm run build; cd ../../..;
+
+#iterate
+#for dir in services/rest; do (cd "$dir" && npm i && npm run build); done
+#for dir in services/events/ethereum/*; do (cd "$dir" && npm i && npm run build); done
+#for dir in services/ingesters/*; do (cd "$dir" && npm i && npm run build); done
