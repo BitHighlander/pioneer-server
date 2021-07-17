c1135ce0cefc03e6f1be82b7c8751c46ae662fb1
highlander <bithighlander@gmail.com>
2021-07-17 12:30:21 -0600

    use latest

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,8 +1,8 @@
 #verbose
-cd services/rest && npm i && npm run build; cd ../..;
 cd services/ingesters/pubkeys && npm i && npm run build; cd ../../..;
 cd services/ingesters/txs/high && npm i && npm run build; cd ../../../..;
 cd services/ingesters/txs/low && npm i && npm run build; cd ../../../..;
+cd services/rest && npm i && npm run build; cd ../..;
 
 #iterate
 #for dir in services/rest; do (cd "$dir" && npm i && npm run build); done
