97b3fc929688d46bc07e2880d7d4e43e6dbc677a
bithighlander <bithighlander@gmail.com>
2023-09-17 13:25:56 -0500

    nerf explore

--- services/rest/src-api/controllers/pioneer.controller.ts
+++ services/rest/src-api/controllers/pioneer.controller.ts
@@ -52,8 +52,8 @@ blocknative.init()
 
 
 //explore
-let explore = require('@pioneer-platform/pioneer-explore')
-explore.init({})
+// let explore = require('@pioneer-platform/pioneer-explore')
+// explore.init({})
 
 //rest-ts
 import { Body, Controller, Get, Post, Route, Tags, SuccessResponse, Query, Request, Response, Header } from 'tsoa';
@@ -309,77 +309,77 @@ export class pioneerController extends Controller {
      *    Build an atlas on a new Dapp
      *
      * */
-    @Post('pioneer/query')
-    public async query(@Header('Authorization') authorization: string, @Body() body: any): Promise<any> {
-        let tag = TAG + " | query | "
-        let timingResults = {};
-        try{
-            let startTime = Date.now();
-
-            log.debug(tag,"Query Body:  ",body)
-            if(!body.query) throw Error("query is required!")
-
-            let authTimeStart = Date.now();
-            const authInfo = await redis.hgetall(authorization)
-            timingResults['auth'] = Date.now() - authTimeStart;
-            if(Object.keys(authInfo).length === 0) throw Error("You must register to use Query!")
-            log.debug("authInfo: ",authInfo)
-
-            let userDataTimeStart = Date.now();
-            const userData = await redis.get(authorization + ":user");
-            log.debug("userData: ",userData)
-            timingResults['userData'] = Date.now() - userDataTimeStart;
-
-            //username
-
-            //@TODO streaming search pushs
-            //assign queryId
-            //push events to stream to username
-
-            //get summary of query
-
-            //get all related assets
-
-            //get all related blockchains
-
-            //get all related nodes
-
-            //get all related dapps
-
-            //use db to build response
-            let memory = ["data.txt"]
-            let loadKnowledgeTimeStart = Date.now();
-            await explore.loadKnowledge(memory)
-            timingResults['loadKnowledge'] = Date.now() - loadKnowledgeTimeStart;
-
-            if(userData){
-                await explore.loadString(userData)
-            }
-
-            let queryTimeStart = Date.now();
-            let query = body.query
-            log.debug("query: ",query)
-            if(!query) throw Error("Must have query!")
-            let response = await explore.query(body.query)
-            if(!response) throw Error("Failed to build response!")
-            timingResults['query'] = Date.now() - queryTimeStart;
-            timingResults['total'] = Date.now() - startTime;
-
-            let output = {
-                response,
-                timings: timingResults
-            }
-
-            return output
-        } catch(e){
-            let errorResp:Error = {
-                success:false,
-                tag,
-                e
-            }
-            log.error(tag,"e: ",{errorResp})
-            throw new ApiError("error",503,"error: "+e.toString());
-        }
-    }
+    // @Post('pioneer/query')
+    // public async query(@Header('Authorization') authorization: string, @Body() body: any): Promise<any> {
+    //     let tag = TAG + " | query | "
+    //     let timingResults = {};
+    //     try{
+    //         let startTime = Date.now();
+    //
+    //         log.debug(tag,"Query Body:  ",body)
+    //         if(!body.query) throw Error("query is required!")
+    //
+    //         let authTimeStart = Date.now();
+    //         const authInfo = await redis.hgetall(authorization)
+    //         timingResults['auth'] = Date.now() - authTimeStart;
+    //         if(Object.keys(authInfo).length === 0) throw Error("You must register to use Query!")
+    //         log.debug("authInfo: ",authInfo)
+    //
+    //         let userDataTimeStart = Date.now();
+    //         const userData = await redis.get(authorization + ":user");
+    //         log.debug("userData: ",userData)
+    //         timingResults['userData'] = Date.now() - userDataTimeStart;
+    //
+    //         //username
+    //
+    //         //@TODO streaming search pushs
+    //         //assign queryId
+    //         //push events to stream to username
+    //
+    //         //get summary of query
+    //
+    //         //get all related assets
+    //
+    //         //get all related blockchains
+    //
+    //         //get all related nodes
+    //
+    //         //get all related dapps
+    //
+    //         //use db to build response
+    //         let memory = ["data.txt"]
+    //         let loadKnowledgeTimeStart = Date.now();
+    //         await explore.loadKnowledge(memory)
+    //         timingResults['loadKnowledge'] = Date.now() - loadKnowledgeTimeStart;
+    //
+    //         if(userData){
+    //             await explore.loadString(userData)
+    //         }
+    //
+    //         let queryTimeStart = Date.now();
+    //         let query = body.query
+    //         log.debug("query: ",query)
+    //         if(!query) throw Error("Must have query!")
+    //         let response = await explore.query(body.query)
+    //         if(!response) throw Error("Failed to build response!")
+    //         timingResults['query'] = Date.now() - queryTimeStart;
+    //         timingResults['total'] = Date.now() - startTime;
+    //
+    //         let output = {
+    //             response,
+    //             timings: timingResults
+    //         }
+    //
+    //         return output
+    //     } catch(e){
+    //         let errorResp:Error = {
+    //             success:false,
+    //             tag,
+    //             e
+    //         }
+    //         log.error(tag,"e: ",{errorResp})
+    //         throw new ApiError("error",503,"error: "+e.toString());
+    //     }
+    // }
 
 }
